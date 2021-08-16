05c064cecc04eb7d96f2d175cc7a273faa0a0463
zeljkosamed <zeljkosamed@users.noreply.github.com>
2021-08-16 15:47:37 +0300

    CarDto düzenlendi ve RentalDto eklendi.

--- DataAccess/Concrete/EntityFramework/EfRentalDal.cs
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -1,13 +1,40 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
     public class EfRentalDal : EfEntityRepositoryBase<Rental, ReCapContext>, IRentalDal
     {
+        public List<RentalDetailDto> GetRentalDetails()
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                var result = from r in context.Rentals
+                             join c in context.Cars
+                             on r.CarId equals c.CarId
+                             join u in context.Users
+                             on r.CustomerId equals u.Id
+                             join b in context.Brands
+                             on c.BrandId equals b.BrandId
+                             select new RentalDetailDto
+                             {
+                                 RentalId = r.RentalId,
+                                 RentDate = r.RentDate,
+                                 ReturnDate = r.ReturnDate,
+                                 BrandName = b.BrandName,
+                                 FirstName = u.FirstName,
+                                 LastName = u.LastName
+                             };
+                return result.ToList();
+
+            }
+        }
     }
 }
